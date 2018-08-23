02a6260d8cabe354aa2318e03e7631382db863e5
randytia <randytia.akbar@gmail.com>
2018-08-23 14:16:40 +0700

    commint

---
+++ app/src/main/java/com/randy/wisatasemarang/adapter/WisataAdapter.java
@@ -0,0 +1,8 @@
+package com.randy.wisatasemarang.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+public class WisataAdapter extends RecyclerView.Adapter<WisataAdapter.WisataViewHolder>{
+    
+} {
+}
